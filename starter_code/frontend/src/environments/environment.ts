6242c2a052001fc3d7779f5d49fc1000feb19190
XW <50823387+XueWang2019@users.noreply.github.com>
2021-02-22 12:00:11 +0100

    add .eu to domain at frontend

--- starter_code/frontend/src/environments/environment.ts
+++ starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://localhost:5000', // the running FLASK api server url
   auth0: {
-    url: 'coffeeshopxw', // the auth0 domain prefix
+    url: 'coffeeshopxw.eu', // the auth0 domain prefix
     audience: 'http://localhost:5000', // the audience set for the auth0 app
     clientId: 'yx3co8sqj3uWJNNe1N6OxWLT7dY4ZqlF', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
